ff5d2e61a5336dc8f92bebc0b9c7e9f56d8e1117
Kagami <shimura.kyohei7617@gmail.com>
2024-05-04 14:58:43 +0900

    auto: auto command push

--- src/stackover.rs
+++ src/stackover.rs
@@ -4,11 +4,22 @@ pub fn run() {
     // stack overflow
     // 配列のstack要件(8MB)を超えると発生
 
-    // 7MB(safe)
-    let _a1: [u8; 7000000] = [1; 7000000];
+    // // 7MB(safe)
+    // let _a1: [u8; 7000000] = [1; 7000000];
 
-    // 9MB(stack overflow)
-    let _a2: [u8; 9000000] = [1; 9000000];
+    // // 9MB(stack overflow)
+    // let _a2: [u8; 9000000] = [1; 9000000];
+
+    // Vector型
+    let mut v1 = vec![1, 2, 3, 4];
+    let v2 = vec![5, 6, 7, 8];
+    let mut v3 = vec![9, 10];
+
+    // Vector型とString型はメモリ構造が同じ(ptr(8bytes), len(8bytes), capacity(8bytes))
+    println!(
+        "Stack address of\nv1 is: {:p}\nv2 is: {:p}\nv3 is: {:p}",
+        &v1, &v2, &v3
+    );
 
     println!("vars.rs is done!\n------");
 }
