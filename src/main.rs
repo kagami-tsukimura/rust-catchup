719a0643f62c4e662ab23302eb503fa38a52985c
Kagami <shimura.kyohei7617@gmail.com>
2024-05-04 14:47:58 +0900

    auto: auto command push

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ fn main() {
     // println!("Hello, world!");
     // println!("Rust is awesome!");
 
-    // vars.rsのrun()を実行
-    vars::run();
+    // // vars.rsのrun()を実行
+    // vars::run();
     // vars::sub_a::func_a();
 
-    ownership::run();
+    // ownership::run();
 
     stackover::run();
 }
