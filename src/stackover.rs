aaa9fc2bdb5cf2f79d1ccc8547549762bec803f9
Kagami <shimura.kyohei7617@gmail.com>
2024-05-04 15:03:32 +0900

    auto: auto command push

--- src/stackover.rs
+++ src/stackover.rs
@@ -36,5 +36,18 @@ pub fn run() {
         &v2.capacity()
     );
 
+    // insert
+    v1.insert(1, 10);
+    println!("{:?}", v1);
+
+    // Len
+    println!("Len of \nv1 is: {}\nv2 is: {}", &v1.len(), &v2.len());
+    // Cap
+    println!(
+        "Capacity of \nv1 is: {}\nv2 is: {}",
+        &v1.capacity(),
+        &v2.capacity()
+    );
+
     println!("vars.rs is done!\n------");
 }
