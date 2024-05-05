28c3503a0f931c620148146bb90acc80f483ef7f
Kagami <shimura.kyohei7617@gmail.com>
2024-05-05 21:16:03 +0900

    auto: auto command push

--- src/enums.rs
+++ src/enums.rs
@@ -1,13 +1,17 @@
 #[derive(Debug)]
 enum OS {
     Windows(u32, String),
-    MacOS(u32, String),
+    Mac(u32, String),
     Linux(u32, String),
 }
 
 pub fn run() {
     println!("enums.rs is start!");
     let linux = OS::Linux(1991, String::from("Linus"));
+    let windows = OS::Windows(1985, String::from("Microsoft"));
+    let mac = OS::Mac(1985, String::from("Apple"));
 
+    println!("windows = {:?}", windows);
+    println!("mac = {:?}", mac);
     println!("linux = {:?}", linux);
 }
