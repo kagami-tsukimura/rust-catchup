a4bc466e08437fac5b632fba568c8a0109a250c9
Kagami <shimura.kyohei7617@gmail.com>
2024-05-05 23:52:49 +0900

    auto: auto command push

--- src/hand.rs
+++ src/hand.rs
@@ -5,6 +5,14 @@ pub fn run() {
     // Result型: エラーが発生する可能性がある値をエラーハンドリング
     // →Error時のパニック（処理の停止）をせずにハンドリング可能
 
+    let res1 = division_option(5.0, 2.0);
+    match res1 {
+        // 正常系: if y != 0.0
+        Some(x) => println!("Result: {:.3}", x),
+        // 異常系: if y == 0.0
+        None => println!("Error: Division by zero."),
+    }
+
     println!("hand.rs is done!\n------");
 }
 
