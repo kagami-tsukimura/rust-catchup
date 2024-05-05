39ca6ce9a55a1f51dc2bd6250739d135df8902cc
Kagami <shimura.kyohei7617@gmail.com>
2024-05-05 23:48:51 +0900

    auto: auto command push

--- src/hand.rs
+++ src/hand.rs
@@ -1,6 +1,8 @@
 pub fn run() {
     println!("hand.rs is start!");
 
-    // option型: 型が存在しないものをエラーハンドリング
+    // Option型: 値が存在しないものをエラーハンドリング
+    // Result型: エラーが発生する可能性がある値をエラーハンドリング
+
     println!("hand.rs is done!\n------");
 }
