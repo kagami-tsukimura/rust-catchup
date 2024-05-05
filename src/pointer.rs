8b4ac4a933e998f63a6b97c6bc1886e86488589f
Kagami <shimura.kyohei7617@gmail.com>
2024-05-05 10:39:45 +0900

    auto: auto command push

--- src/pointer.rs
+++ src/pointer.rs
@@ -1,4 +1,5 @@
 pub fn run() {
+    println!("pointer.rs is start!");
     // Boxポインタ
     // スタックに存在するデータをそのままヒープに移動
     // 移動したデータのポインタを、Boxポインタとしてスタックに保持
@@ -13,7 +14,12 @@ pub fn run() {
     // スタック：Boxポインタ（heapデータの先頭アドレスポインタ）
     // ヒープ：データ、スタックデータ、ptr(8bytes), len(8bytes), capacity(8bytes)
 
-    println!("pointer.rs is start!");
+    let t1: (i64, String) = (10, String::from("hello"));
+    println!("Stack address of t1 is: {:p}", &t1);
+    // ヒープのアドレス（"hello"）のptr(8bytes), len(8bytes), capacity(8bytes)取得
+    println!("Heap memory address of t1 is: {:?}", &t1.1.as_ptr());
+    println!("Len of t1 is: {}", &t1.1.len());
+    println!("Capacity of t1 is: {}", &t1.1.capacity());
 
     println!("pointer.rs is done!\n------");
 }
