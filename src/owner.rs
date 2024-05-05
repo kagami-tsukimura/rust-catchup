25a885a5381ff1cad7237c7547deea1a3521d368
Kagami <shimura.kyohei7617@gmail.com>
2024-05-05 11:09:28 +0900

    auto: auto command push

--- src/owner.rs
+++ src/owner.rs
@@ -1,8 +1,9 @@
 pub fn run() {
     println!("owner.rs is start!");
 
-    //
+    // 所有権：s1
     let s1 = String::from("hello");
+    // 所有権：s1 → s2
     let s2 = s1;
     // println!("{}, {}", s1, s2);
 
