7ef6799e0df2aed3001c963b14508efe5da49de8
Kagami <shimura.kyohei7617@gmail.com>
2024-05-05 11:09:55 +0900

    auto: auto command push

--- src/owner.rs
+++ src/owner.rs
@@ -5,6 +5,7 @@ pub fn run() {
     let s1 = String::from("hello");
     // 所有権：s1 → s2
     let s2 = s1;
+    // s1に所有権がない（s2にmove）ためエラー
     // println!("{}, {}", s1, s2);
 
     println!("owner.rs is done!\n------");
