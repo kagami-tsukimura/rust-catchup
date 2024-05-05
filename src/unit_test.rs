5f80fd17c2f14eb3b2d2e51260c542dd443de3ec
Kagami <shimura.kyohei7617@gmail.com>
2024-05-06 08:43:03 +0900

    auto: auto command push

--- src/unit_test.rs
+++ src/unit_test.rs
@@ -1,3 +1,15 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    // 1, 2引数の面積を比較し、1が大きければtrueを返す
+    fn compare_area(&self, other: &Rectangle) -> bool {
+        self.width * self.height > other.width * other.height
+    }
+}
+
 fn double_value(x: i32) -> i32 {
     x * 2
 }
