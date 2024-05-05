add2c06cf7d3a528db55307a1a13fa953aeea6c0
Kagami <shimura.kyohei7617@gmail.com>
2024-05-05 11:09:07 +0900

    auto: auto command push

--- src/owner.rs
+++ src/owner.rs
@@ -1,8 +1,10 @@
 pub fn run() {
     println!("owner.rs is start!");
 
+    //
     let s1 = String::from("hello");
-    println!("The value of s1 is: {}", s1);
+    let s2 = s1;
+    // println!("{}, {}", s1, s2);
 
     println!("owner.rs is done!\n------");
 }
