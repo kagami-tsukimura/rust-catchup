8af892b0d504694eff68321cc08e87757238d0f7
Kagami <shimura.kyohei7617@gmail.com>
2024-05-05 14:16:01 +0900

    auto: auto command push

--- src/generics.rs
+++ src/generics.rs
@@ -1,3 +1,13 @@
 pub fn run() {
     println!("generics.rs is start!");
 }
+
+fn print_generics(s: &String) {
+    println!("\nprint_generics: {}------", s);
+    println!("{}", s);
+    println!("Stack address of s is: {:p}", &s);
+    println!("Heap address of s is: {:?}", &s.as_ptr());
+    println!("Length of s is: {}", s.len());
+    println!("Capacity of s is: {}", s.capacity());
+    println!("------print_generics: {}\n", s);
+}
