5a2d1372a86cd2b68b1db157870a1aca389185f8
Kagami <shimura.kyohei7617@gmail.com>
2024-05-05 15:30:50 +0900

    auto: auto command push

--- src/ref_borrow.rs
+++ src/ref_borrow.rs
@@ -1,3 +1,12 @@
 pub fn run() {
     println!("ref_borrow.rs is start!");
+
+    // 参照(reference):
+    // 借用(borrowing): 所有権を移動させず、参照権だけを貸す
+    // プリミティブ型は所有権がない→借用の概念なし
+    // - 整数型: i8, i16, i32, i64, i128, u8, u16, u32, u64, u128, isize, usize
+    // - 浮動小数点型: f32, f64
+    // - 論理型: bool
+    // - 文字型: char
+    // - バイト型: u8
 }
