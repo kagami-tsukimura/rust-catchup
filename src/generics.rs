3682e579cd59c0a6c6bbabc28398f7a15fa81fa4
Kagami <shimura.kyohei7617@gmail.com>
2024-05-05 14:27:59 +0900

    auto: auto command push

--- src/generics.rs
+++ src/generics.rs
@@ -2,10 +2,23 @@ pub fn run() {
     let numbers = vec![34, 50, 25, 100, 65];
 
     println!("largest: {}", get_largest_i32(numbers));
+
+    let chars = vec!['a', 'b', 'c', 'd', 'f', 'e'];
+    println!("largest: {}", get_largest_char(chars));
 }
 
 fn get_largest_i32(items: Vec<i32>) -> i32 {
     // Vector<i32>内の最大値を取得
+    let mut largest = items[0];
+    for item in items {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn get_largest_char(items: Vec<char>) -> char {
     let mut largest = items[0];
     for item in items {
         if item > largest {
