de130142ebd9beb4c27e126bd018f2385bc53f12
Kagami <shimura.kyohei7617@gmail.com>
2024-05-05 14:09:27 +0900

    auto: auto command push

--- src/life.rs
+++ src/life.rs
@@ -2,9 +2,17 @@ pub fn run() {
     let x = 5;
     // xのアドレス取得
     let r = &x;
-    println!("{}", x);
     println!("{}", r);
+    println!("{}", x);
 
     // x: 関数をdropする際に削除
     // y: 使用後に削除
+
+    // // ダングリングポインタ
+    // let r;
+    // {
+    //     let x = 5;
+    //     r = &x;
+    // }
+    // println!("r: {}", r);
 }
