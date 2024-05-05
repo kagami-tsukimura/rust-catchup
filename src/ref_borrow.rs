775218ffe3e4c67b84f7056888dc49f31de5c13c
Kagami <shimura.kyohei7617@gmail.com>
2024-05-05 15:32:26 +0900

    auto: auto command push

--- src/ref_borrow.rs
+++ src/ref_borrow.rs
@@ -1,7 +1,8 @@
 pub fn run() {
     println!("ref_borrow.rs is start!");
 
-    // 参照(reference):
+    // 参照(reference): &によるアドレス取得→所有権＋プリミティブ型両方
+    // - String, Vec, Box, Rc, Arc
     // 借用(borrowing): 所有権を移動させず、参照権だけを貸す
     // プリミティブ型は所有権がない→借用の概念なし
     // - 整数型: i8, i16, i32, i64, i128, u8, u16, u32, u64, u128, isize, usize
