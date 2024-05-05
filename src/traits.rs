ee1bcf27fb58a89292f9ed34dc362d4ddb38cf93
Kagami <shimura.kyohei7617@gmail.com>
2024-05-05 21:34:48 +0900

    auto: auto command push

--- src/traits.rs
+++ src/traits.rs
@@ -1,5 +1,18 @@
+// trait: 複数の型に共通の機能を提供
+trait Fruits {
+    fn price(&self) -> u32;
+    fn eat(&self);
+}
+struct Apple;
+impl Fruits for Apple {
+    fn price(&self) -> u32 {
+        100
+    }
+    fn eat(&self) {
+        println!("eat apple");
+    }
+}
+
 pub fn run() {
     println!("trait.rs is start!");
-
-    // traits: 複数の型に共通の機能を提供
 }
