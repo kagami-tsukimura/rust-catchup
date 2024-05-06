605e258fbbf691eccce55fe85d647416f962c071
Kagami <shimura.kyohei7617@gmail.com>
2024-05-06 09:25:52 +0900

    auto: auto command push

--- _library/library_demo/src/lib.rs
+++ _library/library_demo/src/lib.rs
@@ -1,22 +1,7 @@
 mod generator;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 pub fn print_random_number() {
     // gen_ran()はu8型
     let n = generator::gen_ran();
     println!("Random u8: {}", n);
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
